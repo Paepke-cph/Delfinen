017e738e5aa153fcfe15905511d724d289355d84
Benjamin Paepke <cph-bp133@cphbusiness.dk>
2019-05-03 09:46:45 +0200

    missed a error

--- src/storage/DBStorage.java
+++ src/storage/DBStorage.java
@@ -232,9 +232,8 @@ public class DBStorage implements Storage {
     public ArrayList<HashMap<String, String>> getTopFiveCompetitionResultsByDiscipline(int discipline_id) {
         String get_Top_Results_By_Discipline = "SELECT * FROM competi½tion_results join members on ? = members.MEMBER_ID WHERE discipline_id = ? ORDER BY best_time LIMIT 5";
         try (PreparedStatement preparedStatement = sqlConnector.getConnection().prepareStatement(get_Top_Results_By_Discipline)) {
-            preparedStatement.setString(1, "competition_results");
-            preparedStatement.setString(2, "competition_results.MEMBER_ID");
-            preparedStatement.setInt(3, discipline_id);
+            preparedStatement.setString(1, "competition_results.MEMBER_ID");
+            preparedStatement.setInt(2, discipline_id);
             return sqlConnector.selectQuery(preparedStatement);
         } catch (SQLException e) {
             e.printStackTrace();
