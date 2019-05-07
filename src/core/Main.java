084f3d7f057d1f6139bcda69d2184b21c19345a0
Tunoc <random959dude@gmail.com>
2019-05-07 09:59:42 +0200

    Dolphin Sounds

--- src/core/Main.java
+++ src/core/Main.java
@@ -1,9 +1,12 @@
 package core;
 
+import java.io.File;
 import java.sql.SQLException;
 import storage.DBStorage;
-import storage.MockStorage;
 import ui.ConsoleUI;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
 
 /**
  *
@@ -15,11 +18,23 @@ public class Main {
         try {
             DBStorage storage = new DBStorage();
             UIController controller = new UIController(new ConsoleUI(), storage);
+            playDolphinSound();
             controller.startProgram();
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
 
-        
+    private static void playDolphinSound() {
+        try {
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File("Dolphins.wav").getAbsoluteFile());
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioInputStream);
+            clip.loop(1000);
+            clip.start();
+        } catch (Exception ex) {
+            System.out.println("Error with playing sound.");
+            ex.printStackTrace();
+        }
     }
 }
